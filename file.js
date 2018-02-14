33eeb9bea020999dc1c392a9f719e79c474e5725
Tom Coleman <tom@thesnail.org>
2018-02-14 11:12:16 +1100

    branch

--- file.js
+++ file.js
@@ -1,3 +1,19 @@
 function foo() {
   return 'yes!';
 }
+
+function bar() {
+  if (something && somethingElse || bar) {
+    if (innerThing || secondThing && thirdThing) {
+      return whatIt || foobar;
+    }
+  }
+}
+
+function baz() {
+  if (something && somethingElse || bar) {
+    if (innerThing || secondThing && thirdThing) {
+      return whatIt || foobar;
+    }
+  }
+}
