795bd88f150287f40dc5315ccb45d683d8c2059e
Tom Coleman <tom@thesnail.org>
2018-02-14 11:12:16 +1100

    branch

--- file.js
+++ file.js
@@ -5,3 +5,19 @@ function foo() {
     }
   }
 }
+
+function bar() {
+  if (something && somethingElse || bar) {
+    if (innerThing || secondThing && thirdThing) {
+      return whatIt || foobar;
+    }
+  }
+}
+
+function baz() {
+  if (something && somethingElse || bar) {
+    if (innerThing || secondThing && thirdThing) {
+      return whatIt || foobar;
+    }
+  }
+}
